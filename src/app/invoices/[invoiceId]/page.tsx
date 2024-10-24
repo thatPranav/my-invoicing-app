4e42f191bf7640eb3fb00a1921bbd87007429103
thatPranav <jawajipranav@gmail.com>
2024-10-24 19:11:28 +0530

    feat: optimistically update the status of invoice

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,21 +1,9 @@
-import { updateStatusAction } from "@/app/actions";
-import Container from "@/components/Container";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { AVAILABLE_STATUSES } from "@/data/invoices";
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
-import { cn } from "@/lib/utils";
 import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
-import { ChevronDown } from "lucide-react";
 import { notFound } from "next/navigation";
+import Invoice from "./Invoice";
 
 export default async function InvoicePage({
   params,
@@ -42,82 +30,5 @@ export default async function InvoicePage({
     notFound();
   }
 
-  const currentStatus = invoice.status;
-  return (
-    <main className="w-full h-full">
-      <Container>
-        <div className="flex justify-between mb-8">
-          <h1 className="flex items-center gap-4 text-3xl font-semibold">
-            Invoices {invoiceId}
-            <Badge
-              className={cn(
-                "rounded-full",
-                "rounded-full capitalize",
-                currentStatus === "open" && "bg-blue-500",
-                currentStatus === "paid" && "bg-green-600",
-                currentStatus === "void" && "bg-zinc-700",
-                currentStatus === "uncollectible" && "bg-red-600"
-              )}
-            >
-              {invoice.status}
-            </Badge>
-          </h1>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button className="flex items-center gap-2" variant="outline">
-                Change status
-                <ChevronDown className="w-4 h-auto" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              {AVAILABLE_STATUSES.map((status) => {
-                return (
-                  <DropdownMenuItem key={status.id}>
-                    <form action={updateStatusAction}>
-                      <input type="hidden" name="id" value={invoiceId} />
-                      <input type="hidden" name="status" value={status.id} />
-                      <button>{status.label}</button>
-                    </form>
-                  </DropdownMenuItem>
-                );
-              })}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-
-        <p className="text-3xl mb-3">${(invoice.value / 100).toFixed(2)}</p>
-
-        <p className="text-lg mb-8">{invoice.description}</p>
-
-        <h2 className="font-bold text-lg mb-4">Billing Details</h2>
-
-        <ul className="grid gap-2">
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Invoice ID
-            </strong>
-            <span>{invoice.id}</span>
-          </li>
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Invoice Date
-            </strong>
-            <span>{new Date(invoice.createTs).toLocaleDateString()}</span>
-          </li>
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Billing Name
-            </strong>
-            {/* <span>{invoice.customer.name}</span> */}
-          </li>
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Billing Email
-            </strong>
-            {/* <span>{invoice.customer.email}</span> */}
-          </li>
-        </ul>
-      </Container>
-    </main>
-  );
+  return <Invoice invoice={invoice} />;
 }
