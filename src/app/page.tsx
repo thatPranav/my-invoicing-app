dbc10ffe824d6fd30419439ac7a6701acce7cdf6
thatPranav <jawajipranav@gmail.com>
2024-10-25 12:13:25 +0530

    feat: / page UI fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,9 @@ import Link from "next/link";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center h-screen text-center gap-6 max-w-5xl mx-auto">
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoicipedia</h1>
       <p>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
         <Button asChild>
           <Link href="/dashboard">Sign In</Link>
         </Button>
